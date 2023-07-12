bd114f84887d4b68bf474fae27e390ebfd049d50
NHIDCL <thisisshreya1998@gmail.com>
2023-07-12 12:14:23 +0530

    hello

---
+++ app/src/main/java/com/example/premonsoonaction/addReport.java
@@ -0,0 +1,14 @@
+package com.example.premonsoonaction;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class addReport extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_report);
+    }
+}
