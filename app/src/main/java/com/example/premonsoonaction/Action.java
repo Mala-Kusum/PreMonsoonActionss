ca73e25c60eafcd9d74869b65144a248bf9b4953
Developer <thisisshreya1998@gmail.com>
2023-07-17 13:16:52 +0530

    cccc2

--- app/src/main/java/com/example/premonsoonaction/Action.java
+++ app/src/main/java/com/example/premonsoonaction/Action.java
@@ -8,12 +8,20 @@ import android.view.View;
 import android.widget.Button;
 
 public class Action extends AppCompatActivity {
-    Button equipment;
+    Button equipment,actiontaken;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_action);
         equipment=findViewById(R.id.equipments);
+        actiontaken=findViewById(R.id.actiontaken);
+        actiontaken.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(Action.this,addReport.class);
+                startActivity(i);
+            }
+        });
         equipment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
--- app/src/main/java/com/example/premonsoonaction/addReport.java
+++ app/src/main/java/com/example/premonsoonaction/addReport.java
@@ -1,24 +1,41 @@
 package com.example.premonsoonaction;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 
 public class addReport extends AppCompatActivity {
     Button next;
+    ImageButton addvuner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_report);
         next=findViewById(R.id.next);
+        Dialog dialog = new Dialog(addReport.this);
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 setContentView(R.layout.page2_addreport);
             }
         });
+        addvuner=findViewById(R.id.addvuner);
+        addvuner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.setContentView(R.layout.dialog);
+                dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                dialog.setCancelable(true);
+                dialog.show();
+            }
+        });
     }
+
 }
