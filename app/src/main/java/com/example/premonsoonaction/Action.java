cb57917bf680abc0d2ea3be7ee51d3149248da0c
Developer <thisisshreya1998@gmail.com>
2023-07-19 15:57:30 +0530

    cccc2

--- app/src/main/java/com/example/premonsoonaction/Action.java
+++ app/src/main/java/com/example/premonsoonaction/Action.java
@@ -6,18 +6,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Action extends AppCompatActivity {
-    Button equipment,actiontaken;
+    Button equipment,actiontaken,material,raterunning;
+    TextView t;
+    public static String selectedAction;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_action);
         equipment=findViewById(R.id.equipments);
         actiontaken=findViewById(R.id.actiontaken);
+        material=findViewById(R.id.materials);
+        raterunning=findViewById(R.id.raterunning);
+        t=findViewById(R.id.no);
         actiontaken.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                selectedAction="Report";
                 Intent i=new Intent(Action.this,Report.class);
                 startActivity(i);
             }
@@ -25,6 +32,26 @@ public class Action extends AppCompatActivity {
         equipment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                selectedAction="Equipment";
+                t.setText("No.");
+                Intent i=new Intent(Action.this,Equipments.class);
+                startActivity(i);
+            }
+        });
+        material.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectedAction="Material";
+                t.setText("Quantity.");
+                Intent i=new Intent(Action.this,Equipments.class);
+                startActivity(i);
+            }
+        });
+        raterunning.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectedAction="Rate running";
+                t.setText("No.");
                 Intent i=new Intent(Action.this,Equipments.class);
                 startActivity(i);
             }
