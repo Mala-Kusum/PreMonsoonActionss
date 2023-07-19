927cdb3756a92b2afea0ddebbf4e6c575bd36a4a
Developer <thisisshreya1998@gmail.com>
2023-07-19 15:26:14 +0530

    cccc2

--- app/src/main/java/com/example/premonsoonaction/addReport2.java
+++ app/src/main/java/com/example/premonsoonaction/addReport2.java
@@ -8,17 +8,34 @@ import android.view.View;
 import android.widget.ImageButton;
 
 public class addReport2 extends AppCompatActivity {
-    ImageButton addvuner;
+    ImageButton addvuner,addcritical,inspected,warning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_report2);
         addvuner=findViewById(R.id.addvuner);
+        addcritical=findViewById(R.id.addcritical);
+        inspected=findViewById(R.id.addinsp);
+        warning=findViewById(R.id.addwarn);
         addvuner.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 DialogFragment newFragment = new adddialog();
+                newFragment.show(getSupportFragmentManager(), "game");
+            }
+        });
+        addcritical.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogFragment newFragment = new adddialog();
+                newFragment.show(getSupportFragmentManager(), "game");
+            }
+        });
+        inspected.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogFragment newFragment = new adddialog2();
                 newFragment.show(getSupportFragmentManager(), "game");
             }
         });
