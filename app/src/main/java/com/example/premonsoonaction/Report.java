5e2b00a3811dd5dcf085f0319d3768a6a0cc4cc4
Developer <thisisshreya1998@gmail.com>
2023-07-19 17:56:49 +0530

    cccc2

--- app/src/main/java/com/example/premonsoonaction/Report.java
+++ app/src/main/java/com/example/premonsoonaction/Report.java
@@ -1,15 +1,18 @@
 package com.example.premonsoonaction;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class Report extends AppCompatActivity {
     FloatingActionButton button;
+    ImageButton from,to;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +23,20 @@ public class Report extends AppCompatActivity {
             public void onClick(View view) {
                 Intent i=new Intent(Report.this,addReport.class);
                 startActivity(i);
+            }
+        });
+        from.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogFragment newFragment = new DatePick();
+                newFragment.show(getSupportFragmentManager(), "datePicker");
+            }
+        });
+        to.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogFragment newFragment = new DatePick();
+                newFragment.show(getSupportFragmentManager(), "datePicker");
             }
         });
     }
