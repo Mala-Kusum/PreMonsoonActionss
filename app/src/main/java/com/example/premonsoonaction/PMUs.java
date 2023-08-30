b44f7a30bce303df0f8b158f1afca8b0574ec878
NHIDCL <thisisshreya1998@gmail.com>
2023-08-30 17:58:24 +0530

    hello 30 aug

--- app/src/main/java/com/example/premonsoonaction/PMUs.java
+++ app/src/main/java/com/example/premonsoonaction/PMUs.java
@@ -1,14 +1,20 @@
 package com.example.premonsoonaction;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class PMUs extends AppCompatActivity {
 
+    RecyclerView grid;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pmus);
+        grid=findViewById(R.id.grid);
+        placard_adapter ad=new placard_adapter(ROs.this,);
+        grid.setAdapter(ad);
     }
 }
