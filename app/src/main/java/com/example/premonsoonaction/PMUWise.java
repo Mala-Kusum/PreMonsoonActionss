f75868099e8e780a53743396970afc4b5b6a2738
NHIDCL <thisisshreya1998@gmail.com>
2023-09-01 17:20:04 +0530

    hello 30 aug

--- app/src/main/java/com/example/premonsoonaction/PMUWise.java
+++ app/src/main/java/com/example/premonsoonaction/PMUWise.java
@@ -6,15 +6,18 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.TextView;
 
 import com.anthonyfdev.dropdownview.DropDownView;
 
 public class PMUWise extends AppCompatActivity {
     DropDownView report;
+    TextView action_type;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pmuwise);
+        action_type = findViewById(R.id.action_type);
         /*report = (DropDownView) findViewById(R.id.report);
         View shrink = LayoutInflater.from(this).inflate(R.layout.report_header,null,false);
         View  expanded = LayoutInflater.from(this).inflate(R.layout.report_footer,null,false);
