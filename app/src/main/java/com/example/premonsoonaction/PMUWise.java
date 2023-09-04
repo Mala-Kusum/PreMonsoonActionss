3f2a775acf0b872dbdf0ae5d31d9dae16248bcf3
NHIDCL <thisisshreya1998@gmail.com>
2023-09-04 12:49:01 +0530

    hello 30 aug

--- app/src/main/java/com/example/premonsoonaction/PMUWise.java
+++ app/src/main/java/com/example/premonsoonaction/PMUWise.java
@@ -1,11 +1,13 @@
 package com.example.premonsoonaction;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.anthonyfdev.dropdownview.DropDownView;
@@ -13,11 +15,99 @@ import com.anthonyfdev.dropdownview.DropDownView;
 public class PMUWise extends AppCompatActivity {
     DropDownView report;
     TextView action_type;
+    TextView rate,equip,mater,actionreport;
+    LinearLayout ratel,equipl,materl,reportl;
+    CardView ratec,equipc,materc,reportc;
+    Boolean expanded,expandedeq,expandedmat,expandedrep;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pmuwise);
-        action_type = findViewById(R.id.action_type);
+        ratec=findViewById(R.id.raterunningcard);
+        rate= findViewById(R.id.raterunning);
+        ratel = findViewById(R.id.raterunninglayout);
+        ratel.setVisibility(View.GONE);
+        equipc=findViewById(R.id.equipmentcard);
+        equip=findViewById(R.id.equipment);
+        equipl=findViewById(R.id.equipmentlayout);
+        equipl.setVisibility(View.GONE);
+        materc=findViewById(R.id.materialcard);
+        mater=findViewById(R.id.material);
+        materl=findViewById(R.id.materiallayout);
+        materl.setVisibility(View.GONE);
+        reportc=findViewById(R.id.reportcard);
+        actionreport=findViewById(R.id.report);
+        reportl=findViewById(R.id.reportlayout);
+        reportl.setVisibility(View.GONE);
+
+        expandedeq=false;
+        expandedmat=false;
+        expanded=false;
+        expandedrep=false;
+
+        equipc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(expandedeq){
+                    equip.setVisibility(View.VISIBLE);
+                    equipl.setVisibility(View.GONE);
+                    expandedeq=false;
+                }
+                else{
+                    equip.setVisibility(View.GONE);
+                    equipl.setVisibility(View.VISIBLE);
+                    expandedeq=true;
+                }
+            }
+        });
+
+        materc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(expandedmat){
+                    mater.setVisibility(View.VISIBLE);
+                    materl.setVisibility(View.GONE);
+                    expandedmat=false;
+                }
+                else{
+                    mater.setVisibility(View.GONE);
+                    materl.setVisibility(View.VISIBLE);
+                    expandedmat=true;
+                }
+            }
+        });
+        ratec.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(expanded){
+                    rate.setVisibility(View.VISIBLE);
+                    ratel.setVisibility(View.GONE);
+                    expanded=false;
+                }
+                else{
+                    rate.setVisibility(View.GONE);
+                    ratel.setVisibility(View.VISIBLE);
+                    expanded= true;
+                }
+            }
+        });
+
+        reportc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(expandedrep){
+                    actionreport.setVisibility(View.VISIBLE);
+                    reportl.setVisibility(View.GONE);
+                    expandedrep=false;
+                }
+                else{
+                    actionreport.setVisibility(View.GONE);
+                    reportl.setVisibility(View.VISIBLE);
+                    expandedrep=true;
+                }
+            }
+        });
+        //action_type = findViewById(R.id.action_type);
         /*report = (DropDownView) findViewById(R.id.report);
         View shrink = LayoutInflater.from(this).inflate(R.layout.report_header,null,false);
         View  expanded = LayoutInflater.from(this).inflate(R.layout.report_footer,null,false);
